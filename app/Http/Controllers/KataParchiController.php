fb77a21e03525741cbeecbf9ea6ddad987419e29
sukhraj <sukhrajverma2003@gmail.com>
2025-06-30 13:52:41 +0530

    kata parchi done

--- app/Http/Controllers/KataParchiController.php
+++ app/Http/Controllers/KataParchiController.php
@@ -51,4 +51,40 @@ class KataParchiController extends Controller
         DB::update("UPDATE kata_parchi SET is_deleted = 1 where kp_id = $id");
         return redirect::to ('kataparchi')->with('success','kataparchi deleted successfully.');
     }
+
+    function edit($id) {
+        $data['kataparchi'] = DB::select("select * from kata_parchi where kp_id = '$id' and is_deleted = 0");
+        return view('kataparchi/edit',$data);
+
+        //return redirect::to ('kataparchi')->with('success','kataparchi update successfully.');
+    } 
+
+    function update(Request $req) {
+       
+        $kpdate = date('Y-m-d', strtotime($req->input('kp_date')));
+        $kpacc = $req->input('kp_acc_no');
+        $kprel = $req->input('kp_rel_name');
+        $kpacc_hold_name = $req->input('kp_acc_holdername');
+        $kp_land_owner = $req->input('kp_bhoomiswami_name');
+        $kpvilage = $req->input('kp_vilage');
+        $kp_acre = $req->input('kp_rakaba_acre');
+        $kpmn = $req->input('kp_mo_no');
+        $kp_rogger = $req->input('kp_rogger_name');
+        $kpvarity = $req->input('kp_varity');
+        $kprst = $req->input('kp_rstno');
+        $kp_vwihgt = $req->input('kp_vechicle_wight');
+        $kp_goween = $req->input('kp_godown_name');
+
+        $id = $req->input('kp_id');
+
+        
+        DB::update("update kata_parchi set 
+
+        kp_date ='$kpdate',kp_acc_no ='$kpacc',kp_rel_name ='$kprel',kp_acc_holdername ='$kpacc_hold_name',kp_bhoomiswami_name='$kp_land_owner',kp_vilage ='$kpvilage',kp_rakaba_acre='$kp_acre',kp_mo_no= '$kpmn',kp_rogger_name='$kp_rogger',kp_verity='$kpvarity',kp_rstno= '$kprst',kp_vehicle_wight='$kp_vwihgt',kp_godown_name='$kp_goween'  where kp_id='$id' ");
+                
+       
+
+        return Redirect::to('kataparchi');
+    }
+
 }
--- resources/views/kataparchi/edit.blade.php
+++ resources/views/kataparchi/edit.blade.php
@@ -1,139 +1,155 @@
-{{ Form::open(['url' => 'kataparchi/add', 'method' => 'post', 'class'=>'needs-validation','novalidate']) }}
+{{ Form::open(['url' => 'kataparchi/update', 'method' => 'put', 'class'=>'needs-validation','novalidate']) }}
 <div class="modal-body">
-    <h6 class="sub-title">kataparchi</h6>
+    <h6 class="sub-title">Kataparchi</h6>
 
     <div class="row">
+            
 
-        <div class="col-lg-6 col-md-6 col-sm-6">
+           <div class="col-lg-12 col-md-12 col-sm-12">
+ 
+
+        
+
+             <div class="col-lg-6 col-md-6 col-sm-6">
             <div class="form-group">
                 <label for="kp_date" class="form-label">date</label>
                 <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required" name="kp_date" type="text" id="kp_date">
-                </div>
-            </div>
-        </div>
+                    <input class="form-control alwaysvisible"
+                    required
+                    name="kp_date"
+                    type="text"
+                    id="kp_date"
+                    value="{{$kataparchi[0]->kp_date}}"
+                    >
 
-        <div class="col-lg-6 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label for="kp_acc_no" class="form-label">account no.</label>
-                <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required, digit" name="kp_acc_no" type="number" id="kp_acc_no">
-                </div>
-            </div>
-        </div>
-
-        
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_rel_name" class="form-label">  customer name </label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_rel_name" type="text" id="kp_rel_name">
-                </div>
-            </div>
-        </div>
-
-         <div class="col-lg-6 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label for="kp_acc_holdername" class="form-label">account holder name</label>
-                <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required, digit" name="kp_acc_holdername" type="text" id="kp_acc_holdername">
-                </div>
-            </div>
-        </div>
-
-        
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_bhoomiswami_name" class="form-label"> land owner name</label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_bhoomiswami_name" type="text" id="kp_bhoomiswami_name">
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_vilage" class="form-label">villagel</label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_vilage" type="text" id="kp_vilage">
-                </div>
-            </div>
-        </div>
-
-         <div class="col-lg-6 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label for="kp_rakaba_acre" class="form-label"> land acre</label>
-                <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required, digit" name="kp_rakaba_acre" type="text" id="kp_rakaba_acre">
-                </div>
-            </div>
-        </div>
-
-        
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_mo_no" class="form-label">Mobile no. </label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_mo_no" type="number" id="kp_mo_no">
-                </div>
-            </div>
-        </div>
-
-         <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_rogger_name" class="form-label">Rogating agent name</label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_rogger_name" type="text" id="kp_rogger_name">
-                </div>
-            </div>
-        </div>
-
-         <div class="col-lg-6 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label for="kp_varity" class="form-label"> variety</label>
-                <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required, digit" name="kp_varity" type="text" id="kp_varity">
-                </div>
-            </div>
-        </div>
-
-        
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
-            <div class="form-group">
-                <label for="kp_rstno" class="form-label"> RST no</label>
-                <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_rstno" type="text" id="kp_rstno">
                 </div>
             </div>
         </div>
 
           <div class="col-lg-6 col-md-6 col-sm-6">
             <div class="form-group">
-                <label for="kp_vechicle_wight" class="form-label">  vehcle weight</label>
+                <label for="kp_acc_no" class="form-label">account no.</label>
                 <div class="form-icon-user">
-                    <input class="form-control alwaysvisible" required="required, digit" name="kp_vechicle_wight" type="number" id="kp_vechicle_wight">
+                     <input class="form-control "
+                    required
+                    name="kp_acc_no"
+                    type="text"
+                    id="kp_acc_no"
+                    value="{{$kataparchi[0]->kp_acc_no}}"
+                    >
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_rel_name" class="form-label">Customer Name</label>
+                <div class="form-icon-user">
+                     <input class="form-control alwaysvisible" required pattern="[A-Za-z ]+" title="Only alphabets are allowed" name="kp_rel_name" type="text" id="kp_rel_name" value="{{$kataparchi[0]->kp_rel_name}}">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_acc_holdername" class="form-label">Account Holder Name</label>
+                <div class="form-icon-user">
+                    <input class="form-control alwaysvisible" required="required" name="sellto_acc_holder" type="text" id="kp_acc_holdername" value="{{$kataparchi[0]->kp_acc_holdername}}">
                 </div>
             </div>
         </div>
 
         
-        <div class="col-lg-12 col-md-12 col-sm-12 onlyforformesrs">
+        <div class="col-lg-6 col-md-6 col-sm-6 changehide">
+            <div class="form-group">
+                <label for="kp_bhoomiswami_name" class="form-label">land owner name</label>
+                <div class="form-icon-user">
+                     <input class="form-control " required="required" name="kp_bhoomiswami_name" type="text" id="kp_bhoomiswami_name" value="{{$kataparchi[0]->kp_bhoomiswami_name}}">
+                </div>
+            </div>
+        </div>
+
+         <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_vilage" class="form-label"> Village </label>
+                <div class="form-icon-user">
+                    <input class="form-control " required pattern="[A-Za-z ]+" title="Only letters allowed" name="kp_vilage" type="text" id="kp_vilage" value="{{$kataparchi[0]->kp_vilage	}}">
+                </div>
+            </div>
+        </div>
+        
+       
+
+         <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_rakaba_acre" class="form-label">land acre</label>
+                <div class="form-icon-user">
+                    <input class="form-control " required="required" name="kp_rakaba_acre" type="text" id="kp_rakaba_acre" value="{{$kataparchi[0]->kp_rakaba_acre}}">
+                </div>
+            </div>
+        </div>
+
+         <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_mo_no" class="form-label"> Mobile no.  </label>
+                <div class="form-icon-user">
+                    <input class="form-control " required="required" name="kp_mo_no" type="text" id="kp_mo_no" value="{{$kataparchi[0]->kp_mo_no}}">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_rogger_name" class="form-label"> Rogating agent name </label>
+                <div class="form-icon-user">
+                    <input class="form-control " required pattern="[A-Za-z ]+" title="Only letters allowed" name="kp_rogger_name" type="text" id="kp_rogger_name" value="{{$kataparchi[0]->kp_rogger_name	}}">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_varity" class="form-label">variety </label>
+                <div class="form-icon-user">
+                    <input class="form-control " required pattern="[A-Za-z ]+" title="Only letters allowed" name="kp_varity" type="text" id="kp_varity" value="{{$kataparchi[0]->kp_verity}}">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_rstno" class="form-label"> RST no </label>
+                <div class="form-icon-user">
+                    <input class="form-control "  title="Only letters allowed" name="kp_rstno" type="text" id="kp_rstno" value="{{$kataparchi[0]->kp_rstno}}">
+                </div>
+            </div>
+        </div>
+
+         <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="form-group">
+                <label for="kp_vehicle_wight" class="form-label">vehcle weight </label>
+                <div class="form-icon-user">
+                    <input class="form-control "  title="Only letters allowed" name="kp_vehicle_wight" type="text" id="kp_vehicle_wight" value="{{$kataparchi[0]->kp_vehicle_wight}}">
+                </div>
+            </div>
+        </div>
+
+         <div class="col-lg-6 col-md-6 col-sm-6">
             <div class="form-group">
                 <label for="kp_godown_name" class="form-label"> Godown name</label>
                 <div class="form-icon-user">
-                    <input class="form-control onlyforformesrs" required="required" name="kp_godown_name" type="text" id="kp_godown_name">
+                    <input class="form-control " required pattern="[A-Za-z ]+" title="Only letters allowed" name="kp_godown_name" type="text" id="kp_godown_name" value="{{$kataparchi[0]->kp_godown_name}}">
                 </div>
             </div>
         </div>
-       
-       
-       
         
-
+      <input type="hidden" name="kp_id" value="{{$kataparchi[0]->kp_id}}">
+        
     </div>
 </div>
 <div class="modal-footer">
     <input type="button" value="Cancel" class="btn btn-light" data-bs-dismiss="modal">
-    <input type="submit" value="Create" class="btn btn-primary">
+    <input type="submit" value="Update" class="btn btn-primary">
 </div>
 </form>
+
