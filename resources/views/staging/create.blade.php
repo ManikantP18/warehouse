73624f9be82a0e92fdfb7465d5f963119e67568d
paramsapna <sapukushwah@gmail.com>
2025-07-03 15:38:21 +0530

    sapna created staging

--- resources/views/staging/create.blade.php
+++ resources/views/staging/create.blade.php
@@ -73,3 +73,19 @@
     <input type="submit" value="Create" class="btn btn-primary">
 </div>
 </form>
+
+<script>
+    (function () {
+        'use strict';
+        var forms = document.querySelectorAll('.needs-validation');
+        Array.from(forms).forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    })();
+</script>
